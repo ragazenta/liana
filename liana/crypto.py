db7adf4789031712a38277b5cd51eb969919e751
Renjaya Raga Zenta <ragazenta@gmail.com>
2022-11-30 08:40:09 +0700

    Update crypto

--- liana/crypto.py
+++ liana/crypto.py
@@ -12,37 +12,39 @@ from cryptography.hazmat.primitives.serialization import (
 )
 
 
-def generate_ec256_privkey() -> bytes:
-    privkey = ec.generate_private_key(ec.SECP256R1())
-    return privkey.private_bytes(Encoding.PEM, PrivateFormat.PKCS8, NoEncryption())
+def generate_ec256_privkey() -> ec.EllipticCurvePrivateKey:
+    return ec.generate_private_key(ec.SECP256R1())
 
 
-def generate_x25519_privkey() -> bytes:
-    privkey = x25519.X25519PrivateKey.generate()
-    return privkey.private_bytes(Encoding.PEM, PrivateFormat.PKCS8, NoEncryption())
+def generate_x25519_privkey() -> ec.EllipticCurvePrivateKey:
+    return x25519.X25519PrivateKey.generate()
 
 
-def generate_ed25519_privkey() -> bytes:
-    privkey = ed25519.Ed25519PrivateKey.generate()
-    return privkey.private_bytes(Encoding.PEM, PrivateFormat.PKCS8, NoEncryption())
+def generate_ed25519_privkey() -> ec.EllipticCurvePrivateKey:
+    return ed25519.Ed25519PrivateKey.generate()
 
 
 def load_privkey(pem_privkey: bytes) -> ec.EllipticCurvePrivateKey:
     return serialization.load_pem_private_key(pem_privkey, password=None)
 
 
+def export_privkey(privkey: ec.EllipticCurvePrivateKey) -> bytes:
+    return privkey.private_bytes(Encoding.PEM, PrivateFormat.PKCS8, NoEncryption())
+
+
+def export_pubkey(pubkey: ec.EllipticCurvePublicKey) -> bytes:
+    return pubkey.public_bytes(Encoding.PEM, PublicFormat.SubjectPublicKeyInfo)
+
+
 def encrypt_ec256(data: bytes, pubkey: ec.EllipticCurvePublicKey) -> bytes:
-    privkey = ec.generate_private_key(ec.SECP256R1())
+    privkey = generate_ec256_privkey()
     sharedkey = privkey.exchange(ec.ECDH(), pubkey)
     chacha = ChaCha20Poly1305(sharedkey)
     nonce = os.urandom(12)
     cipher = bytearray()
     cipher.extend(nonce)
     cipher.extend(chacha.encrypt(nonce, data, None))
-    cipherpub = privkey.public_key().public_bytes(
-        encoding=Encoding.PEM,
-        format=PublicFormat.SubjectPublicKeyInfo,
-    )[27:152]
+    cipherpub = export_pubkey(privkey.public_key())[27:152]
     return base64.b64encode(cipher) + cipherpub[:64] + cipherpub[65:]
 
 
@@ -66,17 +68,14 @@ def decrypt_ec256(cipher: bytes, privkey: ec.EllipticCurvePrivateKey) -> bytes:
 
 
 def encrypt_x25519(data: bytes, pubkey: ec.EllipticCurvePublicKey) -> bytes:
-    privkey = x25519.X25519PrivateKey.generate()
+    privkey = generate_x25519_privkey()
     sharedkey = privkey.exchange(pubkey)
     chacha = ChaCha20Poly1305(sharedkey)
     nonce = os.urandom(12)
     cipher = bytearray()
     cipher.extend(nonce)
     cipher.extend(chacha.encrypt(nonce, data, None))
-    cipherpub = privkey.public_key().public_bytes(
-        encoding=Encoding.PEM,
-        format=PublicFormat.SubjectPublicKeyInfo,
-    )[27:87]
+    cipherpub = export_pubkey(privkey.public_key())[27:87]
     return base64.b64encode(cipher) + cipherpub
 
 
