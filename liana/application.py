6a7079591b095e994c3d6529f61e9c0226794d29
majiidmuhammad <majiid831@gmail.com>
2022-11-30 17:06:03 +0700

    perbaikan create, pembuatan update

--- liana/application.py
+++ liana/application.py
@@ -5,9 +5,11 @@ from flask import (
 )
 
 from liana.db import get_db
+from . import crypto
 
 bp = Blueprint("app", __name__)
 
+                # Index
 
 @bp.route("/")
 def index():
@@ -21,10 +23,73 @@ def index():
     apps = cur.fetchall()
     return render_template("index.html", apps=apps)
 
+                # End Index
+
+                # Create
 
 @bp.route("/create", methods=("GET", "POST"))
 def create():
-    return "Under construction"
+    if request.method == 'POST':
+        appcode = request.form['AppCode']
+        algorithm = request.form['Algorithm']
+        # PrivateKey = request.form['PrivateKey']
+        # SignatureKey = request.form['SignatureKey']
+        createdby = request.form['CreatedBy']
+        error = None
+
+        if not appcode:
+            error = 'AppCode is required.'
+        
+        if algorithm == "EC256":
+            signaturekey = crypto.export_privkey(crypto.generate_ec256_privkey())
+            privatekey = crypto.export_privkey(crypto.generate_ec256_privkey())
+        
+        elif algorithm == "Ed25519":
+            signaturekey = crypto.export_privkey(crypto.generate_ed25519_privkey())
+            privatekey = crypto.export_privkey(crypto.generate_x25519_privkey())
+
+        else:
+            error = 'invalid Algorithm '
+
+        if error is not None:
+            flash(error)
+        else:
+            db = get_db()
+            cur= db.cursor(dictionary=True)
+            cur.execute(
+                'INSERT INTO Application (AppCode, SignatureKey, PrivateKey, Algorithm, CreatedBy)'
+                ' VALUES (%s, %s, %s, %s, %s)',
+                (appcode, signaturekey, privatekey, algorithm, createdby)
+            )
+            db.commit()
+            return redirect(url_for('index'))
+
+    return render_template('create.html')
+
+def get_post():
+    cur = get_db().cursor()
+    cur.execute(
+        "SELECT a.AppCode AS code, a.Algorithm AS algorithm, 0 AS lic"
+        " FROM Application a"
+        " ORDER BY a.AppCode ASC"
+    )
+    aps =cur.fetchone()
+
+    if aps:
+        return aps
+
+                # End Create
+
+                # Update
+        
+@bp.route('/update', methods=('GET', 'POST'))
+def update():
+    app = get_post()
+
+    return render_template('update.html', post=app)
+
+
+                # End Update        
 
 
 @bp.route("/<appcode>")
