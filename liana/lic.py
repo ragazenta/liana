2d370d9731e6dd764859ea86838c145d88474dc9
majiidmuhammad <majiid831@gmail.com>
2022-12-21 08:44:49 +0700

    Merge branch 'main' of https://github.com/majiidmuhammad/liana

--- liana/lic.py
+++ liana/lic.py
@@ -7,7 +7,7 @@ ALGORITHMS = {
 }
 
 
-def generate(payload, signkey, algorithm) -> bytes:
+def generate(payload, signkey: bytes, algorithm: str) -> bytes:
     if algorithm not in ALGORITHMS:
         return "invalid_algorithm"
 
@@ -18,3 +18,19 @@ def generate(payload, signkey, algorithm) -> bytes:
         array.append(0x0A)
     array.extend(b"-----END LICENSE-----\n")
     return bytes(array)
+
+
+def decode(content: str, lickey: bytes, algorithm: str) -> dict:
+    if algorithm not in ALGORITHMS:
+        return {
+            "invalid": "invalid_algorithm",
+        }
+
+    lines = content.encode("ascii").split(b"\n")
+    token = b"".join(lines[1 : len(lines) - 2])
+    try:
+        return jwt.decode(token, lickey.encode("ascii"), ALGORITHMS[algorithm])
+    except Exception as e:
+        return {
+            "error": str(e),
+        }
